4b444b96eb4bd0f2a7c3db23fd6a6ef71ec95b35
ZiYang <ziyang@ZiYangdeMacBook-Pro.local>
2016-11-22 17:54:16 +0800

    123

---
+++ qq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qq
+//
+//  Created by ZiYang on 16/11/22.
+//  Copyright (c) 2016年 ZiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ qq/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  qq
+//
+//  Created by ZiYang on 16/11/22.
+//  Copyright (c) 2016年 ZiYang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"123");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
